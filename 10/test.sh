17a1a081e010a08fcafaa7bfa53969596de4e575
Yuya Isaka <zxwg6329@gmail.com>
2023-05-29 21:05:26 +0900

    変数できた

---
+++ 10/test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+assert() {
+	expected="$1"
+	input="$2"
+
+	./isakacc "$input" > tmp.s || exit
+	gcc -o tmp tmp.s
+	./tmp
+	actual="$?"
+
+	if [ "$expected" = "$actual" ]; then
+		echo "$input => $actual"
+	else
+		echo "$input => $expected expected, but got $actual"
+	fi
+}
+
+assert 10 '10;'
+assert 150 '150;'
+assert 37 '10 + 30 -3;'
+assert 24 '10+20-3-3;'
+assert 150 '(10 + 20) * 5;'
+assert 19 '10 +3 *3;'
+assert 20 '30 - 10 /5 * (3+2);'
+assert 20 '- - - - 20;'
+assert 31 '- + - 30 + - 3 - - 4;'
+assert 7 '- 30 + 20 - - 10 + -3 -- 10;'
+assert 1 '1 < 10;'
+assert 0 '10 < 10;'
+assert 0 '10 < -11;'
+assert 0 '10 > 10;'
+assert 1 '10 > 9;'
+assert 0 '-10 > 9;'
+assert 1 '10 >= 9;'
+assert 1 '10 >= 10;'
+assert 0 '10 >= 11;'
+assert 1 '10 <= 11;'
+assert 1 '11 <= 11;'
+assert 0 '12 <= 11;'
+assert 7 'a=3; b=4; a+b;'
+assert 18 'a=1; b=3+4; c=4-1; d = 10-3+2-2*1/2; z = c*d; u = z - b; y = a + u; y;'
---
+++ 10/tokenize.c
@@ -0,0 +1,106 @@
+#include "isakacc.h"
+
+static char *user_input;
+
+void error(char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(EXIT_FAILURE);
+}
+
+static void verror(char *at, char *fmt, va_list ap)
+{
+	int len = at - user_input;
+
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", len, "");
+	fprintf(stderr, "^ ");
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	va_end(ap);
+	exit(EXIT_FAILURE);
+}
+
+void error_tok(Token *tok, char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	verror(tok->loc, fmt, ap);
+}
+
+void error_at(char *at, char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	verror(at, fmt, ap);
+}
+
+static Token *new_token(TokenKind kind, char *start, char *end)
+{
+	Token *tok = calloc(1, sizeof(Token));
+	tok->kind = kind;
+	tok->loc = start;
+	tok->len = end - start;
+	return tok;
+}
+
+static bool start_with(char *p, char *target)
+{
+	return strncmp(p, target, strlen(target)) == 0;
+}
+
+static int read_punct(char *p)
+{
+	if (start_with(p, "==") || start_with(p, "!=") || start_with(p, "<=") || start_with(p, ">="))
+		return 2;
+
+	return ispunct(*p) ? 1 : 0;
+}
+
+Token *tokenize(char *p)
+{
+	user_input = p;
+	Token head = {};
+	Token *cur = &head;
+
+	while (*p)
+	{
+		if (isspace(*p))
+		{
+			p++;
+			continue;
+		}
+
+		if (isdigit(*p))
+		{
+			cur = cur->next = new_token(TK_NUM, p, p);
+			char *tmp = p;
+			cur->val = strtoul(p, &p, 10);
+			cur->len = p - tmp;
+			continue;
+		}
+
+		if ('a' <= *p && *p <= 'z')
+		{
+			cur = cur->next = new_token(TK_IDENT, p, p + 1);
+			p++;
+			continue;
+		}
+
+		int punct_len = read_punct(p);
+		if (punct_len)
+		{
+			cur = cur->next = new_token(TK_PUNCT, p, p + punct_len);
+			p += punct_len;
+			continue;
+		}
+
+		error_at(p, "error tokenize\n");
+	}
+
+	cur = cur->next = new_token(TK_EOF, p, p);
+	return head.next;
+}
