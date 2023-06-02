ecbfd179dcb364d200b03048168cefa183f82ced
Yuya Isaka <zxwg6329@gmail.com>
2023-06-03 02:02:26 +0900

    OK

---
+++ 11/isakacc.h
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+
+typedef enum
+{
+	TK_VAR,
+	TK_PUNCT,
+	TK_NUM,
+	TK_EOF
+} TokenKind;
+
+typedef struct Token Token;
+struct Token
+{
+	TokenKind kind;
+	Token *next;
+	int val;
+	char *loc;
+	int len;
+};
+
+typedef enum
+{
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_NUM,
+	ND_NEG,
+	ND_EQ,
+	ND_NE,
+	ND_LT,
+	ND_LE,
+	ND_EXPR_STMT,
+	ND_VAR,
+	ND_ASSIGN
+} NodeKind;
+
+typedef struct Node Node;
+struct Node
+{
+	NodeKind kind;
+	Node *next;
+	Node *lhs;
+	Node *rhs;
+	int val;
+	char name;
+};
+
+void error_tok(Token *tok, char *fmt, ...);
+void error(char *fmt, ...);
+Token *tokenize(char *p);
+Node *parse(Token *tok);
+void codegen(Node *node);
